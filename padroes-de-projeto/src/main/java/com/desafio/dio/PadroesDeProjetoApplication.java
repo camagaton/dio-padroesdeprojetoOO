e0fe2368add97a13576574827175c253f5b7104b
camagaton <camagaton@gmail.com>
2022-10-21 07:34:15 -0300

    criacao do projeto no spring.io

---
+++ padroes-de-projeto/src/main/java/com/desafio/dio/PadroesDeProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoApplication.class, args);
+	}
+
+}
